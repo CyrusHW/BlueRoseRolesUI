a059ccf630bdc306bc73df0c21829ead3aa6e32e
CyrusHW <cyrushwells@gmail.com>
2026-04-16 11:53:45 -0700

    Make Index work

--- app/src/main/java/blue/rose/App.java
+++ app/src/main/java/blue/rose/App.java
@@ -3,12 +3,27 @@
  */
 package blue.rose;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.concurrent.Worker;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class App extends Application{
+    private WebEngine engine;
+
+    public static void main(String[] args) throws IOException{
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    @Override
+    public void start(Stage stage){
+        WebView view = new WebView();
+        engine = view.getEngine();
+        engine.load(getClass().getResource("/Index.html").toExternalForm());
+        
     }
 }
