94bf2b7e94c67863ba30eb4a7b4b9ffacbed1895
CyrusHW <cyrushwells@gmail.com>
2026-04-22 13:38:49 -0700

    Made somewhat work

---
+++ app/src/main/java/blue/rose/Constants.java
@@ -0,0 +1,14 @@
+package blue.rose;
+
+public class Constants {
+    public static int teamMembers = 5;
+    public static int windowWidth = 900;
+    public static int windowHeight = 550;
+
+    public static int mapWidth = 800;
+    public static double mapPadding = 100;
+
+    // from constants.java in robot code
+    public static final double fieldWidth = 8.069326;
+    public static final double fieldLength = 16.540988;
+}
---
+++ app/src/main/java/blue/rose/FileManager.java
@@ -0,0 +1,5 @@
+package blue.rose;
+
+public class FileManager {
+    
+}
